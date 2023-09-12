ed508a0c88b4e75a65ea66ef3ebd0d5639077267
Eugene Cross <eugene.cross@gmail.com>
2023-09-12 17:26:32 +0100

    Initial Commit

---
+++ VoiceOver/VoiceOverApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceOverApp.swift
+//  VoiceOver
+//
+//  Created by Eugene on 12/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct VoiceOverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

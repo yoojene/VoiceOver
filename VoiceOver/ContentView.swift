d6f151ede2ea20fd6fa80601ed1ee946ed9e9f79
Eugene Cross <eugene.cross@gmail.com>
2023-09-13 10:48:07 +0100

    Accessibility - identifying views with labels

--- VoiceOver/ContentView.swift
+++ VoiceOver/ContentView.swift
@@ -8,12 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+    
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    
+    let labels = [
+    "Tulips", "Frozen tree buds", "Sunflowers", "Fireworks"
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image(pictures[selectedPicture])
+                .resizable()
+                .scaledToFit()
+                .onTapGesture {
+                    selectedPicture = Int.random(in: 0...3)
+                }
+                .accessibilityLabel(labels[selectedPicture]) //spoken immediatly
+                .accessibilityAddTraits(.isButton)
+                //.accessibilityRemoveTraits(<#T##traits: AccessibilityTraits##AccessibilityTraits#>) // remove 
+//                .accessibilityHint(<#T##hint: Text##Text#>) // after a short delay after label
         }
         .padding()
     }
